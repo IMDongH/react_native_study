67364bbc7c76b54ecd0da6ba5da74b387be9c5d5
dong <idh1007@naver.com>
2022-03-14 10:00:55 +0900

    init

---
+++ LoginPractice/App.js
@@ -0,0 +1,3 @@
+import App from 'C:\Users\idh10\LoginPractice\src\New_App';
+
+export default App;
---
+++ LoginPractice/index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from 'C:\Users\idh10\LoginPractice\src\New_App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
